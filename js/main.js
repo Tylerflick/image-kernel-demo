61d4ed95042e4a8e7df0c7d0a48f0841a019a0e5
Tyler Hoeflicker <tyler.hoeflicker@gmail.com>
2018-06-04 12:01:03 -0700

    working

--- js/main.js
+++ js/main.js
@@ -1,22 +1,71 @@
-let image = "https://images.unsplash.com/photo-1464219789935-c2d9d9aba644?ixlib=rb-0.3.5&s=16151b3854690e644cc07dcc1e7e8e71&auto=format&fit=crop&w=4350&q=80";
+let imageAddress = "assets/1.png"
 
 function main() {
-    let gl = setupContext();
-    let program = setupShaders(gl);
-    WebGlUtils.resize(gl);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    // Clear the canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.useProgram(program);
-    gl.enableVertexAttribArray(gl.getAttribLocation(program, "a_position"));
-    render(image, gl, program);
+    var image = new Image();
+    image.src = imageAddress;
+    image.onload = function () {
+        if (image === null) {
+            console.log("Image is null");
+            return;
+        }
+        let canvas = document.getElementById("Canvas");
+        let gl = setupContext(canvas);
+        let program = setupShaders(gl);
+
+        gl.useProgram(program);
+        var positionLocation = gl.getAttribLocation(program, "a_position");
+
+        var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+    
+        var texCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    
+        gl.enableVertexAttribArray(texCoordLocation);
+        gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+    
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+    
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    
+        setRectangle(gl, 0.0, 0.0, 1.0, 1.0);
+
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+
+        var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+        gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+    
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    
+        setRectangle(gl, image.x, image.y, image.width, image.height);
+    
+        // draw
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
 }
 
-function setupContext() {
-    const canvas = document.getElementById("Canvas");
+function setRectangle(gl, x, y, width, height) {
+    var x2 = x + width;
+    var y2 = y + height;
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(
+        [x, y,
+            x2, y,
+            x, y2,
+            x, y2,
+            x2, y,
+            x2, y2
+        ]), gl.STATIC_DRAW);
+}
+
+function setupContext(canvas) {
     // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+    const gl = WebGLDebugUtils.makeDebugContext(canvas.getContext("webgl"));
 
     // Only continue if WebGL is available and working
     if (!gl) {
@@ -25,53 +74,13 @@ function setupContext() {
     }
 
     // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
     return gl;
 }
 
 function setupShaders(gl) {
     const program = WebGlUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
-
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-    var positionBuffer = gl.createBuffer();
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    var positions = [0, 0];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
     return program
 }
-
-function render(image, gl, program) {
-    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
-   
-    // provide texture coordinates for the rectangle.
-    var texCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        0.0,  0.0,
-        1.0,  0.0,
-        0.0,  1.0,
-        0.0,  1.0,
-        1.0,  0.0,
-        1.0,  1.0]), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(texCoordLocation);
-    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
-   
-    // Create a texture.
-    var texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-   
-    // Set the parameters so we can render any size image.
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-   
-    // Upload the image into the texture.
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  }
-
